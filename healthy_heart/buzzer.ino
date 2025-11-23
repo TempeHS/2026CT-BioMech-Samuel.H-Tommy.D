a64693d50394c96ee1285d21b4e82bc220bcaf6b
tommytech1 <tommy.dockrill@education.nsw.gov.au>
2025-11-24 09:04:29 +1100

    deletion of buzzer

--- healthy_heart/buzzer.ino
+++ healthy_heart/buzzer.ino
@@ -1,27 +1,4 @@
-/*
-  Purpose: Play a tone on Grove Buzzer
-  Hardware: Grove Buzzer on D3 (PWM pin)
-  Notes:
-    - Use tone() for melodies
-    - Connect to D3, D5, D6, D9, D10, or D11 for PWM/tone
-*/
 
-const int BUZZER_PIN = 3;  // Grove D3 (PWM)
 
-void setup() {
-  Serial.begin(9600);
-  pinMode(BUZZER_PIN, OUTPUT);
-  Serial.println("Buzzer test ready");
-}
-
-void loop() {
-  // Play 1kHz tone for 500ms
-  tone(BUZZER_PIN, 1000);  // 1000 Hz
-  delay(500);
-
-  // Stop tone for 500ms
-  noTone(BUZZER_PIN);
-  delay(500);
-}
 
 
