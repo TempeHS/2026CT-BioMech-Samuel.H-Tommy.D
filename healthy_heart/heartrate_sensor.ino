5ac71aacdd53ef0808ec59ef6263e06ec2b076ce
Samuel H <samuel.haszard@education.nsw.gov.au>
2025-11-26 09:19:54 +1100

    heartrate ssensor change to add buzzer

--- healthy_heart/heartrate_sensor.ino
+++ healthy_heart/heartrate_sensor.ino
@@ -1,14 +1,20 @@
 #include <Wire.h>
+
+volatile int heartRate = 0;  // Global variable - Tab 2 will read this
+
 void setup() {
     Serial.begin(9600);
     Serial.println("heart rate sensor:");
     Wire.begin();
 }
+
 void loop() {
-    Wire.requestFrom(0xA0 >> 1, 1);    // request 1 bytes from slave device
-    while(Wire.available()) {          // slave may send less than requested
-        unsigned char c = Wire.read();   // receive heart rate value (a byte)
-        Serial.println(c, DEC);         // print heart rate value
+    Wire.requestFrom(0xA0 >> 1, 1);    // request 1 byte from I2C address 0x50
+    while (Wire.available()) {         
+        unsigned char c = Wire.read();
+        heartRate = (int)c;             // Update global heartRate variable
+        Serial.print("HR: ");
+        Serial.println(heartRate, DEC);
     }
     delay(500);
 }
