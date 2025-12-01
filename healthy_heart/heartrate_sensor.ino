87e112e66d6b61501f22a78c76213aa56286e294
Samuel H <samuel.haszard@education.nsw.gov.au>
2025-12-01 12:30:15 +1100

    ,.

--- healthy_heart/heartrate_sensor.ino
+++ healthy_heart/heartrate_sensor.ino
@@ -1,11 +1,10 @@
 #include <Wire.h>
 
-// CRITICAL: Declare OUTSIDE all functions so Tab 2 can access it
-volatile int heartRate = 0;
+volatile int heartrate = 0;  // lowercase
 
 void setup() {
     Serial.begin(9600);
-    Serial.println("heart rate sensor:");
+    Serial.println("heartrate sensor:");
     Wire.begin();
 }
 
@@ -13,9 +12,9 @@ void loop() {
     Wire.requestFrom(0xA0 >> 1, 1);
     while (Wire.available()) {
         unsigned char c = Wire.read();
-        heartRate = (int)c;  // Update the global variable
+        heartrate = (int)c;  // lowercase
         Serial.print("HR: ");
-        Serial.println(heartRate, DEC);
+        Serial.println(heartrate, DEC);
     }
     delay(500);
 }
