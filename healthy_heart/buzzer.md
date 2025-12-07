7e6e3f749abe93cebb03ed0cfcb8f67f01325129
Samuel H <samuel.haszard@education.nsw.gov.au>
2025-12-08 09:38:35 +1100

    tommy pulls all the huzz

--- healthy_heart/buzzer.md
+++ healthy_heart/buzzer.md
@@ -5,24 +5,3 @@
 We are getting progress with processes in the buzzer.
 
 
-const int BUZZER_PIN = 5;
-const int THRESHOLD_BPM = 150;
-
-void setup() {
-    pinMode(BUZZER_PIN, OUTPUT);
-}
-
-void loop() {
-    int currentHR = heartrate;  // lowercase - MATCHES Tab 1
-
-    if (currentHR > THRESHOLD_BPM) {
-        tone(BUZZER_PIN, 1000);
-        delay(200);
-        noTone(BUZZER_PIN);
-        delay(100);
-    } else {
-        noTone(BUZZER_PIN);
-    }
-
-    delay(100);
-}
--- healthy_heart/heartrate_sensor.ino
+++ healthy_heart/heartrate_sensor.ino
@@ -1,12 +1,9 @@
 #include <Wire.h>
 #define HR_SENSOR_ADDR 0x50  // Confirm in README
 
-const int BUZZER_PIN = 5;    // Grove D5
-
 void setup() {
   Serial.begin(9600);
   Wire.begin();
-  pinMode(BUZZER_PIN, OUTPUT);
   Serial.println("Heart Rate Sensor Test");
 }
 
@@ -16,10 +13,6 @@ void loop() {
     uint8_t hr = Wire.read();
     Serial.print("Heart Rate: ");
     Serial.println(hr);
-
-    if (hr > 150) {
-      tone(BUZZER_PIN, 1000, 200); // 1000 Hz for 200 ms
-    }
   } else {
     Serial.println("No data from sensor");
   }
