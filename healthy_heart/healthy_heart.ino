4c558df95e1b86f91d47fc20bc2d46a2c727c4e5
Samuel H <samuel.haszard@education.nsw.gov.au>
2025-12-09 22:37:25 +0000

    d

--- healthy_heart/healthy_heart.ino
+++ healthy_heart/healthy_heart.ino
@@ -7,7 +7,7 @@ U8G2_SSD1306_128X64_NONAME_F_HW_I2C OLED(U8G2_R0, SCL, SDA, U8X8_PIN_NONE);
 int rate;
 int speaker_access;
 int buttonPIN = 4;
-const int BUZZER_PIN = 5; // Grove D5 (change if using another port)
+const int BUZZER_PIN = 5;  // Grove D5 (change if using another port)
 
 void setup() {
   Serial.begin(9600);
@@ -19,7 +19,7 @@ void setup() {
 void buzzer_setup() {
   pinMode(BUZZER_PIN, OUTPUT);
   // Optionally, play a startup tone
-  tone(BUZZER_PIN, 1000, 200); // 1000 Hz for 200 ms
+  tone(BUZZER_PIN, 1000, 200);  // 1000 Hz for 200 ms
 }
 
 void OLED_screen_setup() {
@@ -32,7 +32,7 @@ void OLED_screen_setup() {
 
 void heartrate_setup() {
   // Example: pinMode for heart rate sensor
-  pinMode(A0, INPUT); // Replace A0 with your sensor pin
+  pinMode(A0, INPUT);  // Replace A0 with your sensor pin
   // Add any other initialization needed for your sensor
 }
 
--- healthy_heart/heartrate_sensor.ino
+++ healthy_heart/heartrate_sensor.ino
@@ -1,13 +1,13 @@
 #include <Wire.h>
 #define HR_SENSOR_ADDR 0x50  // Confirm in README
 
-void setup() {
+void heartrate_sensor_setup() {
   Serial.begin(9600);
   Wire.begin();
   Serial.println("Heart Rate Sensor Test");
 }
 
-void loop() {
+void heartrate_sensor_loop() {
   Wire.requestFrom(HR_SENSOR_ADDR, 1);
   if (Wire.available()) {
     uint8_t hr = Wire.read();
